cdff4e18b16388760860228a2df7066f1b5dfeb0
kaaisz <kaaisuzuki@gmail.com>
2025-07-19 14:37:32 +0900

    Add and update several packages and config files to adapt to Tailwind v4.x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
