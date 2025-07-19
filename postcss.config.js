fcdd42da3006a038416bc09b0b80abb167012973
kaaisz <kaaisuzuki@gmail.com>
2025-07-19 23:33:44 +0900

    Delete postcss.config.mjs and update lines in postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   plugins: {
+    _tailwindcss: {},
+    get tailwindcss() {
+      return this._tailwindcss;
+    },
+    set tailwindcss(value) {
+      this._tailwindcss = value;
+    },
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-export default {
-  plugins: ["@tailwindcss/postcss"],
-};
+// export default {
+//   plugins: ["@tailwindcss/postcss"],
+// };
