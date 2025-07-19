24b7266c1792a52adf91e771d40b3d81ce7ba72e
kaaisz <kaaisuzuki@gmail.com>
2025-07-19 23:37:01 +0900

    Remove unnecessary lines on postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   plugins: {
-    _tailwindcss: {},
-    get tailwindcss() {
-      return this._tailwindcss;
-    },
-    set tailwindcss(value) {
-      this._tailwindcss = value;
-    },
     tailwindcss: {},
     autoprefixer: {},
   },
-}
-// export default {
-//   plugins: ["@tailwindcss/postcss"],
-// };
+};
